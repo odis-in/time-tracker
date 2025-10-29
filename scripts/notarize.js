f208b3971eb930bc1db94ab395b6d7efe2ea38ef
odis-in <info@odis.in>
2025-10-29 12:40:11 -0600

    stable 01

---
+++ scripts/notarize.js
@@ -0,0 +1,16 @@
+// scripts/notarize.js
+const { notarize } = require('@electron/notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') return;
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    appBundleId: 'com.odis.time-tracker',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
+  });
+};
